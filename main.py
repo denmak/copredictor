296930fe2dc43eb8c14762656d306aac91053c25
denmak <46881599+denmak@users.noreply.github.com>
2019-08-23 22:02:47 +0800

    test

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ activation_rates = np.array([0.26, 0.17, 0.12, 0.03, 0.01])
 survival_rates = np.array([1.00,0.88,0.73,0.62,0.47,0.33,0.21,0.12,0.07,0.01])
 
 
+
 CPs = cp.get_prediction_by(cohort = cohort_r,arpu = arpu,ARs = activation_rates, SRs = survival_rates)
 
 print(CPs)
